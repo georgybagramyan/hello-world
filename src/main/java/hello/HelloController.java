e2488bb992523f3c649355a8879b25bc7f50cced
georgybagramyan <georgy.bagramyan@student.kdg.be>
2016-06-21 22:38:01 +0200

    Build on Travis ci 4

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot HEY HEY!";
+        return "Greetings from Spring Boot!";
     }
 
 }
