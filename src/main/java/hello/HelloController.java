af6342850456ccd816fd23aadb03db2841b4a264
georgybagramyan <georgy.bagramyan@student.kdg.be>
2016-06-30 17:22:42 +0200

    Deploy#14

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping(value = "/")
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String index() {
         return "Greetings from Spring Boot!";
     }
