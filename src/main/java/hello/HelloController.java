0a292863c91dc2e7c219e48ed0002d5c025f15c4
georgybagramyan <georgy.bagramyan@student.kdg.be>
2016-06-30 17:17:28 +0200

    Deploy#13
    Added handler in controller - added person class

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,5 +1,7 @@
 package hello;
 
+import com.google.gson.Gson;
+import org.springframework.http.converter.json.GsonFactoryBean;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,18 @@ public class HelloController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping(value = "person",method = RequestMethod.GET)
+    public String person(){
+        Person person = new Person();
+        person.setAdress("Willemp lepelstraat 15, 2000 Antwerpen");
+        person.setAge(27);
+        person.setName("Georgy Bagramyan");
+
+        Gson gson = new Gson();
+
+        return gson.toJson(person);
     }
 
 }
---
+++ src/main/java/hello/Person.java
@@ -0,0 +1,35 @@
+package hello;
+
+/**
+ * Created by lollik on 30/06/2016.
+ */
+public class Person {
+
+    private String name;
+    private int age;
+    private String adress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
